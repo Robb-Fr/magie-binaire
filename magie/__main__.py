7dcaa23aae666754b132f3b8c9cb11223d20ae6b
Robb-Fr <git@robb-i.net>
2023-08-23 15:35:18 +0200

    Uses argparser for main

--- magie/__main__.py
+++ magie/__main__.py
@@ -1,11 +1,18 @@
 """Displays the solution of the card computation"""
-from sys import argv
+import argparse
+from . import all_cards
 
-from . import Card
+parser = argparse.ArgumentParser(
+                    prog='Magie Binaire',
+                    description="Ce programme permet d'afficher les cartes dont il est question dans l'activité magie-binaire de modulo",
+                    epilog='https://enseigner.modulo-info.ch/rep-info/activ/magie_binaire.html')
+
+parser.add_argument("-b", "--bin", action="store_true", help="Si cette option est activée, les nombres seront affichés en format binaire. Sinon ils le seront en format décimal")
+args = parser.parse_args()
 
 for i in reversed(range(6)):
     print(f"\n\n==== Carte {5-i} ====")
-    if len(argv) == 2 and argv[1] == "bin":
-        print(Card(i).display(binary=True))
+    if args.bin:
+        print(all_cards[i].display(binary=True))
     else:
-        print(Card(i).display(binary=False))
+        print(all_cards[i].display(binary=False))
